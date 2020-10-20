65276c8493f8f06a1978c61622c248dd2d14d311
Ernest_Orlov <eriksuper2@gmail.com>
2020-10-20 15:01:23 +0300

    cleaning project

--- Task1/Maven/multi-project/core/src/test/java/by/epam/bigdatalab/UtilsTest.java
+++ Task1/Maven/multi-project/core/src/test/java/by/epam/bigdatalab/UtilsTest.java
@@ -9,22 +9,28 @@ import static org.junit.Assert.assertTrue;
 
 public class UtilsTest
 {
+    private final String POSITIVE_NUMBER = "12";
+    private final String POSITIVE_DOUBLE = "12.0";
+    private final String NEGATIVE_NUMBER = "-12";
+    private final String NEGATIVE_DOUBLE = "-12.0";
+    private final String NOT_NUMBER = "-12asw";
 
     @Test
     public void positiveNumbers()
     {
-        assertTrue( isAllPositiveNumbers("1", "2") );
+        assertTrue( isAllPositiveNumbers(POSITIVE_NUMBER, POSITIVE_DOUBLE) );
     }
 
     @Test
     public void negativeNumbers()
     {
-        assertFalse( isAllPositiveNumbers("-1", "-2") );
+        assertFalse( isAllPositiveNumbers(NEGATIVE_NUMBER, NEGATIVE_DOUBLE) );
     }
 
     @Test
     public void notNumbers()
     {
-        assertFalse( isAllPositiveNumbers("-1as", "-2") );
+        assertFalse( isAllPositiveNumbers(POSITIVE_NUMBER, NOT_NUMBER) );
+        assertFalse( isAllPositiveNumbers(null) );
     }
 }
