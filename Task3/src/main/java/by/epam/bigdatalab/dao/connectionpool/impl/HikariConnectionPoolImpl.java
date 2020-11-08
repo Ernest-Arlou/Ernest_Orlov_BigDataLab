d3cb4030a0ff22d28f8b31b5df8319099160c12c
Ernest_Orlov <eriksuper2@gmail.com>
2020-11-08 07:50:13 +0300

    script

--- Task3/src/main/java/by/epam/bigdatalab/dao/connectionpool/impl/HikariConnectionPoolImpl.java
+++ Task3/src/main/java/by/epam/bigdatalab/dao/connectionpool/impl/HikariConnectionPoolImpl.java
@@ -15,16 +15,27 @@ public final class HikariConnectionPoolImpl implements ConnectionPool {
     @Override
     public void init() {
 
-        Properties props = new Properties();
+//        Properties props = new Properties();
+//
+//        DBResourceManager dbResourceManager = DBResourceManager.getInstance();
+//        props.setProperty(DBParameter.DATA_SOURCE_CLASS_NAME, dbResourceManager.getValue(DBParameter.DB_DATA_SOURCE_CLASS_NAME));
+//        props.setProperty(DBParameter.DATA_SOURCE_USER, dbResourceManager.getValue(DBParameter.DB_USER));
+//        props.setProperty(DBParameter.DATA_SOURCE_PASSWORD, dbResourceManager.getValue(DBParameter.DB_PASSWORD));
+//        props.setProperty(DBParameter.DATA_SOURCE_DATABASE_NAME, dbResourceManager.getValue(DBParameter.DB_NAME));
+//        props.setProperty("dataSource.portNumber", "5432");
+//        props.setProperty("dataSource.serverName", "localhost");
+//
+//
+//        HikariConfig config = new HikariConfig(props);
+//        hikariDataSource = new HikariDataSource(config);
 
-        DBResourceManager dbResourceManager = DBResourceManager.getInstance();
-        props.setProperty(DBParameter.DATA_SOURCE_CLASS_NAME, dbResourceManager.getValue(DBParameter.DB_DATA_SOURCE_CLASS_NAME));
-        props.setProperty(DBParameter.DATA_SOURCE_USER, dbResourceManager.getValue(DBParameter.DB_USER));
-        props.setProperty(DBParameter.DATA_SOURCE_PASSWORD, dbResourceManager.getValue(DBParameter.DB_PASSWORD));
-        props.setProperty(DBParameter.DATA_SOURCE_DATABASE_NAME, dbResourceManager.getValue(DBParameter.DB_NAME));
+//
+        hikariDataSource = new HikariDataSource();
+        hikariDataSource.setJdbcUrl("jdbc:postgresql://localhost:5432/crimes");
+        hikariDataSource.setUsername("postgres");
+        hikariDataSource.setPassword("1234");
 
-        HikariConfig config = new HikariConfig(props);
-        hikariDataSource = new HikariDataSource(config);
+
 
     }
 
